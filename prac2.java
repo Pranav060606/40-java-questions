15e30abe4aa5690a59907334a6222f6811b33f03
Pranav060606 <krishpranav94@gmail.com>
2026-08-18 21:28:07 +0530

    Prac2.java file added

---
+++ prac2.java
@@ -0,0 +1,14 @@
+package startedjava;
+import java.util.*;
+public class prac2 {
+    public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    String str = sc.nextLine();
+    String reversed = "";
+    for(int i = str.length() - 1; i >= 0; i--){
+        reversed += str.charAt(i);
+    }
+    System.out.println(reversed);
+    sc.close();
+}
+}
