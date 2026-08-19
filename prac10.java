48f6d87841aa06736a1c8f2058c01e56bbe98104
Pranav060606 <krishpranav94@gmail.com>
2026-08-19 20:28:16 +0530

    File added

--- prac10.java
+++ prac10.java
@@ -1,18 +1,19 @@
 package startedjava;
 import java.util.*;
-public class prac8 {
+public class prac10 {
     public  static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
         int temp;
+        System.out.print("Enter n: ");
         int n = sc.nextInt();
         for(int i = 0; i < n; i++){
-            System.out.print(b + " ");
             temp = a + b;
             a = b;
             b = temp;
         }
+        System.out.println(a);
         sc.close();
     }
 }
