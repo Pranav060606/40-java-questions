616faca3030084e7a0cffb7d198bfe99356d90c8
Pranav060606 <krishpranav94@gmail.com>
2026-08-21 22:18:51 +0530

    File added

---
+++ prac20.java
@@ -0,0 +1,30 @@
+package startedjava;
+import java.util.*;
+public class prac20 {
+    //Frequency of characters in a string
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        for (int i = 0; i < input.length(); i++) {
+            char currentChar = input.charAt(i);
+            
+            // To prevent counting the same letter twice, we check if it appeared earlier
+            if (input.indexOf(currentChar) == i) {
+                
+                // Count how many times this specific character appears
+                int count = 0;
+                for (int j = 0; j < input.length(); j++) {
+                    if (input.charAt(j) == currentChar) {
+                        count++;
+                    }
+                }
+                if (count > 1) {
+                    // Print the result for this character
+                    System.out.println("Duplicate character: '" + currentChar + "' appears " + count + " times");
+                }
+        }
+        sc.close();
+    }
+}
+}
+
