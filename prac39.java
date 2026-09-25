4a6ccf436c23ccfc4fa75750ea545867051bacad
Pranav060606 <krishpranav94@gmail.com>
2026-09-25 23:30:00 +0530

    File Added

---
+++ prac39.java
@@ -0,0 +1,15 @@
+package startedjava;
+
+public class prac39 {
+      public static void main(String[] args){
+        int[][] arr = {{1,2,3},{4,5,6},{7,8,9}};
+        for(int i = 0; i<arr.length;i++){
+            int sum = 0;
+            for(int j = 0; j<arr[i].length;j++){
+                sum+=arr[j][i];
+            }
+            System.out.println("Sum of column " + i + ": " + sum);
+        }
+    }
+}
+
